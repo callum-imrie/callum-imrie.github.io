fa82192063802684e87b3cabed8bc6abc4e71359
callum-imrie <imrie.callum@icloud.com>
2021-04-10 11:32:34 +0100

    Restructure project, add simple landing page

---
+++ callum-imrie.github.io.models/SelfInformation.cs
@@ -0,0 +1,9 @@
+﻿namespace callum_imrie.github.io.models
+{
+    public class SelfInformation
+    {
+        public const string Name = "Callum Imrie";
+        
+        public const string Title = "Associate Software Engineer";
+    }
+}
